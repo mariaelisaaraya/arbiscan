49a76730a4d19efdd8dea0e50add51490b3c6e94
mariaelisaaraya <arayamariaelisaa@gmail.com>
2024-12-08 00:50:57 -0300

    Add accessMoreInfo and withdrawal functionality

--- packages/hardhat/contracts/AccessMoreInfoContract.sol
+++ packages/hardhat/contracts/AccessMoreInfoContract.sol
@@ -32,6 +32,49 @@ contract AccessMoreInfoContract {
     function setInfoAccessPrice(uint256 newPrice) external onlyOwners {
         infoAccessPrice = newPrice;
     }
+        function accessMoreInfo(address user, address targetContract) external payable {
+        require(user != address(0), "Por favor, proporciona una direccion de usuario valida");
+        require(targetContract != address(0), "Por favor, proporciona una direccion de contrato valida");
+        require(msg.value == infoAccessPrice, "Debes enviar exactamente el monto requerido para acceder a la informacion");
+
+        emit InfoAccessed(user, msg.value, targetContract);
+    }
+
+    function approveWithdrawal() external onlyOwners {
+        require(!withdrawalApprovals[msg.sender], "Ya has aprobado esta accion");
+        withdrawalApprovals[msg.sender] = true;
+        approvalsCount++;
+
+        emit WithdrawInitiated(msg.sender);
+
+        if (approvalsCount == ownerList.length) {
+            _withdraw();
+        }
+    }
+
+    function _withdraw() private {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No hay fondos para retirar");
+        uint256 share = balance / ownerList.length;
+
+        // Transferir la misma cantidad a cada propietario
+        for (uint256 i = 0; i < ownerList.length; i++) {
+            payable(ownerList[i]).transfer(share);
+        }
+
+        // Restablecer las aprobaciones y contador
+        for (uint256 i = 0; i < ownerList.length; i++) {
+            withdrawalApprovals[ownerList[i]] = false;
+        }
+        approvalsCount = 0;
+
+        emit WithdrawCompleted(msg.sender, balance);
+    }
+
+    // Función para consultar el balance actual del contrato
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
     // Permitir que el contrato reciba pagos directamente
     receive() external payable {}
 }
